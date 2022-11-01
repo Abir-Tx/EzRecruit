1964f688a768319d1e4fb9ba810ba033763ffee0
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-11-01 23:30:08 +0600

    Copy & reformat the index from lab task 4
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

---
+++ view/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home | EzRecruiter</title>
+</head>
+
+<body>
+	<?php @include "./pages/header.php" ?>
+	<div class="welcome">
+		<h2>Welcome To EzRecruiter</h2>
+	</div>
+
+	<div class="quickThings">
+		<h3>A Little About Us</h3>
+		<p>
+			<span>E</span>zRecruiter is a digital & smart recruitement website. We are here to make your company or club's recruitment system
+			and hassle easier than ever. Just use our website to make your next recruitment and you will be never leaving this site.
+		</p>
+
+		<div class="things">
+			<div class="blocks">
+				<a href="./pages/registration.php">Sign Up</a>
+			</div>
+
+			<div class="blocks">
+				<a href="./pages/login.php">Login</a>
+			</div>
+		</div>
+
+	</div>
+	<?php @include "./pages/footer.php" ?>
+</body>
+
+</html>
