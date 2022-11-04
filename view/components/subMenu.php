fbcf32fdeb216ceec7320ec1ff8405f67a14fd9f
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-11-04 10:59:57 +0600

    Add submenu and dashboard from lab4
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

---
+++ view/components/subMenu.php
@@ -0,0 +1,15 @@
+<h2>Admin</h2>
+<hr>
+<div class="accountSubmenu">
+	<ul>
+		<li>
+			<a href="./dashboard.php">Dashboard</a>
+		</li>
+		<li><a href="./view.php">View Profile</a></li>
+		<li><a href="./edit.php">Edit Profile</a></li>
+		<li><a href="./changeProPic.php">Change Profile Picture</a></li>
+		<li><a href="./changePass.php">Change Password</a></li>
+		<li><a href="./logout.php">Logout</a></li>
+	</ul>
+</div>
+<hr>
