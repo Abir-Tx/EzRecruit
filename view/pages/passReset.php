b50b4f1466f0b365af7e7742ab7c96f225243236
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-11-06 01:17:31 +0600

    Replace the name with `EzRecruit`
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- view/pages/passReset.php
+++ view/pages/passReset.php
@@ -6,7 +6,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Reset Password | RecruiterX</title>
+	<title>Reset Password | EzRecruit</title>
 	<link rel="stylesheet" href="../styles/css/commons.css">
 </head>
 
