734cc6587594ac3c72d4e483ccc2f038b2789545
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2022-11-07 16:56:02 +0600

    Create a login system using database
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

---
+++ controller/general/loginHandler.php
@@ -0,0 +1,41 @@
+<?php
+@require_once "../../../model/db_connect.php";
+
+$email = $password = "";
+$pass = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// Assignment
+	$email = $_POST["email"];
+	$password = $_POST["pass"];
+
+
+	// DB
+	$conn = db_connect();
+	$selectQuery = "SELECT * FROM users WHERE email = :email AND password = :password";
+	try {
+		$stmt = $conn->prepare($selectQuery);
+		$stmt->execute([
+			":email" => $email,
+			":password" => $password
+		]);
+
+		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		if (count($result) > 0) {
+			$pass = true;
+		}
+	} catch (PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	}
+
+	$conn = null;
+
+	if ($pass) {
+		session_start();
+		$_SESSION["user"] = $result[0];
+		header("Location: ../../index.php");
+	} else {
+		echo "Login failed";
+	}
+}
