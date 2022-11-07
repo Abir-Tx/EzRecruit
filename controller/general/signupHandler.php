16e9dc3b2701df7a18d606f84083f0ed1b7061d3
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2022-11-07 16:40:45 +0600

    Add general password checking
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- controller/general/signupHandler.php
+++ controller/general/signupHandler.php
@@ -2,22 +2,33 @@
 @require_once "../../../model/db_connect.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$name = $email = $password = $cpassword = "";
+	$pass = false;
 
 	// Assignment
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$password = $_POST["pass"];
+	$cpassword = $_POST["cpass"];
+
+	// Check both the password
+	if ($password === $cpassword) {
+		$pass = true;
+	}
 
 	// DB
 	$conn = db_connect();
 	$insertQuery = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
 	try {
 		$stmt = $conn->prepare($insertQuery);
-		$stmt->execute([
-			":name" => $name,
-			":email" => $email,
-			":password" => $password
-		]);
+		if ($pass) {
+			$stmt->execute([
+				":name" => $name,
+				":email" => $email,
+				":password" => $password
+			]);
+
+			echo "User added successfully";
+		}
 	} catch (PDOException $e) {
 		echo "Error: " . $e->getMessage();
 	}
