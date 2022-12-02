4f896862be881beecab5a6ac75cb9f459e7ddcb5
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-12-02 15:13:57 +0600

    Add count of registered recruiters to the website
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- view/pages/admin/dashboard.php
+++ view/pages/admin/dashboard.php
@@ -26,6 +26,29 @@
 	echo "<H1>Welcome " . ucwords($_SESSION['admin']) . "</H1>";
 	?>
 
+	<!-- Show the number of recruiters count from the database -->
+	<?php
+	include_once "../../../model/db_connect.php";
+
+	$conn = db_connect();
+
+	$query = "SELECT COUNT(*) AS count FROM recruiters";
+
+	$result = $conn->query($query);
+
+	$row = $result->fetch(PDO::FETCH_ASSOC);
+
+	$count = $row['count'];
+
+	$conn = null;
+	?>
+
+	<p>
+		Currently there are <?php echo $count ?> registered recruiters on the website
+	</p>
+
+
+
 	<?php @include "../../layout/footer.php" ?>
 </body>
 
