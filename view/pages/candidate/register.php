51a435894bb0c057389f1b0978654ca66761028f
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-12-10 19:08:23 +0600

    [SUCCESS] Add both resume and pro pic upload feature
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- view/pages/candidate/register.php
+++ view/pages/candidate/register.php
@@ -40,7 +40,6 @@
 		$cpassword = $_POST['cpassword'];
 		$shortBio = $_POST['shortBio'];
 		$propic = $_FILES['propic']['name'];
-		$isValid = true;
 
 		// File upload
 		$target_dir = "./profile_pics/";
@@ -87,6 +86,53 @@
 		} else {
 			if (move_uploaded_file($_FILES["propic"]["tmp_name"], $target_file)) {
 				echo "The file " . htmlspecialchars(basename($_FILES["propic"]["name"])) . " has been uploaded.";
+				$isValid = true;
+			} else {
+				echo "Sorry, there was an error uploading your file.";
+			}
+		}
+
+
+		// PDF file upload
+		$target_dir = "./resume/";
+		$target_file = $target_dir . basename($_FILES["resume"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+		// Check if the file is a PDF file or not
+		if (isset($_POST["submit"])) {
+			$check = getimagesize($_FILES["resume"]["tmp_name"]);
+			if ($check !== false) {
+				echo "File is an image - " . $check["mime"] . ".";
+				$uploadOk = 1;
+			} else {
+				echo "File is not an image.";
+				$uploadOk = 0;
+			}
+		}
+
+		// Check if file already exists
+		if (file_exists($target_file)) {
+			echo "Sorry, file already exists.";
+			$uploadOk = 0;
+		}
+
+		// Allow only pdf file format
+		if (
+			$imageFileType != "pdf"
+		) {
+			echo "Sorry, only PDF files are allowed.";
+			$uploadOk = 0;
+		}
+
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+			echo "Sorry, your file was not uploaded.";
+			// if everything is ok, try to upload file
+		} else {
+			if (move_uploaded_file($_FILES["resume"]["tmp_name"], $target_file)) {
+				echo "The file " . htmlspecialchars(basename($_FILES["resume"]["name"])) . " has been uploaded.";
+				$isValid = true;
 			} else {
 				echo "Sorry, there was an error uploading your file.";
 			}
