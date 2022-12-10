dbb261ff67b43c3c8c85699e936332d3d07bff22
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-12-10 19:11:39 +0600

    Remove comments and testing files
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- view/pages/candidate/register.php
+++ view/pages/candidate/register.php
@@ -47,37 +47,11 @@
 		$uploadOk = 1;
 		$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-		// Check if image file is a actual image or fake image
-		if (isset($_POST["submit"])) {
-			$check = getimagesize($_FILES["propic"]["tmp_name"]);
-			if ($check !== false) {
-				echo "File is an image - " . $check["mime"] . ".";
-				$uploadOk = 1;
-			} else {
-				echo "File is not an image.";
-				$uploadOk = 0;
-			}
-		}
-
 		// Check if file already exists
 		if (file_exists($target_file)) {
 			echo "Sorry, file already exists.";
 			$uploadOk = 0;
 		}
-
-		/* 		// Check file size
-		if ($_FILES["propic"]["size"] > 500000) {
-			echo "Sorry, your file is too large.";
-			$uploadOk = 0;
-		}
-
-		// Allow certain file formats
-		if (
-			$imageFileType != "doc" && $imageFileType != "docx" && $imageFileType != "pdf"
-		) {
-			echo "Sorry, only DOC, DOCX & PDF files are allowed.";
-			$uploadOk = 0;
-		} */
 
 		// Check if $uploadOk is set to 0 by an error
 		if ($uploadOk == 0) {
